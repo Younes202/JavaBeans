e787f8c3842ffae32637ca4375b20ca08ab583eb
Younes202 <84544672+Younes202@users.noreply.github.com>
2022-09-13 15:01:02 +0100

    Beans's of Author

--- Beans/src/main/java/Beans/Author.java
+++ Beans/src/main/java/Beans/Author.java
@@ -1,5 +1,27 @@
 package Beans;
 
 public class Author {
+  private String Name;
+  private String Phone;
+  private String Age;
+  public void setName(String Name) {
+		Name = Name;
+	}
+  public String getName(){
+    return Name;
+  }
+	public String getPhone() {
+		return Phone;
+	}
+	public void setPhone(String phone) {
+		Phone = phone;
+	}
+	public int getAge() {
+		return Age;
+	}
+	public void setAge(int age) {
+		Age = age;
+	}
+
 
 }
